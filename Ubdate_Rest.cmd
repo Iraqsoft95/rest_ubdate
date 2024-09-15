c8d2dee141aacbe3870b8c05af4e6c97ba8a78af
Iraqsoft95 <iraqsoft1995@gmail.com>
2024-09-15 14:01:28 +0300

    Update Ubdate_Rest.cmd

--- Ubdate_Rest.cmd
+++ Ubdate_Rest.cmd
@@ -1,4 +1,3 @@
-@ECHO OFF
 @echo off
 set "SCRIPT_PATH=%~dp0"
 set "SCRIPT_NAME=%~nx0"
@@ -10,17 +9,15 @@ curl -o "REST_VVV_PPP.sql" "https://raw.githubusercontent.com/Iraqsoft95/rest_ub
 set SQL_Connecction= 127.0.0.1\SALES_DEV -U sa -P 12345
 set SQL_DATABASE=RESTAURANT_DB
 set SCRIPT =" "
-
+:stert
 sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -i REST_TTT.sql
 sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -i REST_VVV_PPP.sql
 IF %ERRORLEVEL% NEQ 0 (
 sqlcmd -S %SQL_ConnecctionSQL_Connecction% -d %SQL_DATABASE%  -Q "DECLARE @return_value int, @SUBMIT_FLAG int; EXEC @return_value = dbo.P_CHECK_EXIST_COLUMNS @SUBMIT_FLAG = @SUBMIT_FLAG OUTPUT; SELECT @SUBMIT_FLAG as '@SUBMIT_FLAG'; SELECT 'Return Value' = @return_value;"
+goto start
 )
 
 del REST_VVV_PPP.sql
 del REST_TTT.sql
 del %File_Loc%
 pause
-
-
-
