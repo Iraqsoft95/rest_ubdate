83e53d0fc4e7763c897327851bd865b9862c745f
Iraqsoft95 <iraqsoft1995@gmail.com>
2024-09-18 14:53:06 +0300

    Update Ubdate_Rest.cmd

--- Ubdate_Rest.cmd
+++ Ubdate_Rest.cmd
@@ -13,7 +13,7 @@ set SCRIPT =" "
 sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -i REST_TTT.sql
 sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -i REST_VVV_PPP.sql
 IF %ERRORLEVEL% NEQ 0 (
-sqlcmd -S %SQL_ConnecctionSQL_Connecction% -d %SQL_DATABASE%  -Q "DECLARE @return_value int, @SUBMIT_FLAG int; EXEC @return_value = dbo.P_CHECK_EXIST_COLUMNS @SUBMIT_FLAG = @SUBMIT_FLAG OUTPUT; SELECT @SUBMIT_FLAG as '@SUBMIT_FLAG'; SELECT 'Return Value' = @return_value;"
+sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -Q "DECLARE @return_value int, @SUBMIT_FLAG int; EXEC @return_value = dbo.P_CHECK_EXIST_COLUMNS @SUBMIT_FLAG = @SUBMIT_FLAG OUTPUT; SELECT @SUBMIT_FLAG as '@SUBMIT_FLAG'; SELECT 'Return Value' = @return_value;"
 goto start
 )
 
