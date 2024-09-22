d55387a8f689d6a7875b31a21f8c874c027b02ca
Iraqsoft95 <iraqsoft1995@gmail.com>
2024-09-23 00:42:48 +0300

    Update Ubdate_Rest.cmd

--- Ubdate_Rest.cmd
+++ Ubdate_Rest.cmd
@@ -1,23 +1,44 @@
 @echo off
-set "SCRIPT_PATH=%~dp0"
-set "SCRIPT_NAME=%~nx0"
-set File_Loc="%SCRIPT_PATH%%SCRIPT_NAME%"
-
-curl -o "REST_TTT.sql" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/REST_TTT.sql"
-curl -o "REST_VVV_PPP.sql" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/RESTVVV_PPP.sql"
-
+@REM --------------------> Run Batch As Admin <--------------------
+setlocal
+REM check if file Run Us Admin
+openfiles >nul 2>&1
+if '%errorlevel%' == '0' goto main
+powershell -Command "Start-Process cmd -ArgumentList '/c \"%~dp0%~nx0\"' -Verb RunAs"
+exit /b
+@REM --------------------> Varibal <--------------------
+:main
+set "Batch_Path=%~dp0"
+set "Batch_NAME=%~nx0"
+set File_Loc="%Batch_PATH%%Batch_NAME%"
 set SQL_Connecction= .\SALES_DEV -U sa -P 12345
 set SQL_DATABASE=RESTAURANT_DB
-set SCRIPT =" "
+@REM --------------------> Download Files <--------------------
+curl -o "%Batch_Path%/REST_TTT.sql" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/REST_TTT.sql"
+curl -o "%Batch_Path%/REST_VVV_PPP.sql" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/RESTVVV_PPP.sql"
+curl -o "%Batch_Path%/Telegram.Bot.dll" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/Telegram.Bot.dll"
+@REM --------------------> Run Secript <--------------------
 :stert
-sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -i REST_TTT.sql
-sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -i REST_VVV_PPP.sql
+sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -i %Batch_Path%/REST_TTT.sql
+sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE% -i %Batch_Path%/REST_VVV_PPP.sql
 IF %ERRORLEVEL% NEQ 0 (
 sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -Q "DECLARE @return_value int, @SUBMIT_FLAG int; EXEC @return_value = dbo.P_CHECK_EXIST_COLUMNS @SUBMIT_FLAG = @SUBMIT_FLAG OUTPUT; SELECT @SUBMIT_FLAG as '@SUBMIT_FLAG'; SELECT 'Return Value' = @return_value;"
 goto start
 )
-
-del REST_VVV_PPP.sql
-del REST_TTT.sql
+@REM --------------------> Copy File <--------------------
+@REM --------------------> Get Username Fore Use In powershell 
+for /f "delims=" %%A in ('powershell -command "$env:USERNAME"') do set "username=%%A"
+@REM --------------------> Get Shortcut Path
+set "TargetPath="
+for /f "delims=" %%A in ('powershell -command "(New-Object -ComObject WScript.Shell).CreateShortcut('C:\\Users\\%username%\\Desktop\\SPEEDOO REST.lnk').TargetPath"') do set "TargetPath=%%A"
+@REM --------------------> Get The Folder Off Shortcut
+set "TargetDir=%TargetPath%\.." 
+@REM --------------------> copy The File
+copy "%Batch_Path%\Telegram.Bot.dll" "%TargetDir%"
+echo File copied successfully
+del %Batch_Path%\REST_VVV_PPP.sql
+del %Batch_Path%\REST_TTT.sql
+del %Batch_Path%\Telegram.Bot.dll
 del %File_Loc%
+
 pause
