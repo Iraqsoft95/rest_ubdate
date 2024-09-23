f6a4b4eef3c58bf9e7f9e9b1d9619568938be3ea
Iraqsoft95 <iraqsoft1995@gmail.com>
2024-09-23 12:29:03 +0300

    Update Ubdate_Rest.cmd

--- Ubdate_Rest.cmd
+++ Ubdate_Rest.cmd
@@ -1,4 +1,6 @@
 @echo off
+Rest 3.0.5.7  
+TTT & VVV v.90
 @REM --------------------> Run Batch As Admin <--------------------
 setlocal
 REM check if file Run Us Admin
@@ -16,29 +18,31 @@ set SQL_DATABASE=RESTAURANT_DB
 @REM --------------------> Download Files <--------------------
 curl -o "%Batch_Path%/REST_TTT.sql" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/REST_TTT.sql"
 curl -o "%Batch_Path%/REST_VVV_PPP.sql" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/RESTVVV_PPP.sql"
-::curl -o "%Batch_Path%/Telegram.Bot.dll" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/Telegram.Bot.dll"
+curl -o "%Batch_Path%/P_CHECK_EXIST_COLUMNS.sql" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/P_CHECK_EXIST_COLUMNS.sql"
 @REM --------------------> Run Secript <--------------------
 :stert
 sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -i %Batch_Path%/REST_TTT.sql
 sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE% -i %Batch_Path%/REST_VVV_PPP.sql
 IF %ERRORLEVEL% NEQ 0 (
-sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -Q "DECLARE @return_value int, @SUBMIT_FLAG int; EXEC @return_value = dbo.P_CHECK_EXIST_COLUMNS @SUBMIT_FLAG = @SUBMIT_FLAG OUTPUT; SELECT @SUBMIT_FLAG as '@SUBMIT_FLAG'; SELECT 'Return Value' = @return_value;"
+@REM sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -Q "DECLARE @return_value int, @SUBMIT_FLAG int; EXEC @return_value = dbo.P_CHECK_EXIST_COLUMNS @SUBMIT_FLAG = @SUBMIT_FLAG OUTPUT; SELECT @SUBMIT_FLAG as '@SUBMIT_FLAG'; SELECT 'Return Value' = @return_value;"
+sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE% -i %Batch_Path%/P_CHECK_EXIST_COLUMNS.sql
 goto start
 )
 @REM --------------------> Copy File <--------------------
 @REM --------------------> Get Username Fore Use In powershell 
-::for /f "delims=" %%A in ('powershell -command "$env:USERNAME"') do set "username=%%A"
+@REM for /f "delims=" %%A in ('powershell -command "$env:USERNAME"') do set "username=%%A"
 @REM --------------------> Get Shortcut Path
-::set "TargetPath="
-::for /f "delims=" %%A in ('powershell -command "(New-Object -ComObject WScript.Shell).CreateShortcut('C:\\Users\\%username%\\Desktop\\SPEEDOO REST.lnk').TargetPath"') do set "TargetPath=%%A"
+@REM set "TargetPath="
+@REM for /f "delims=" %%A in ('powershell -command "(New-Object -ComObject WScript.Shell).CreateShortcut('C:\\Users\\%username%\\Desktop\\SPEEDOO REST.lnk').TargetPath"') do set "TargetPath=%%A"
 @REM --------------------> Get The Folder Off Shortcut
-::set "TargetDir=%TargetPath%\.." 
+@REM set "TargetDir=%TargetPath%\.." 
 @REM --------------------> copy The File
-::copy "%Batch_Path%\Telegram.Bot.dll" "%TargetDir%"
-::echo File copied successfully
-del %Batch_Path%\REST_VVV_PPP.sql
-del %Batch_Path%\REST_TTT.sql
-del %Batch_Path%\Telegram.Bot.dll
-del %File_Loc%
+@REM copy "%Batch_Path%\Telegram.Bot.dll" "%TargetDir%"
+@REM echo File copied successfully
+@REM --------------------> Delet  File <--------------------
+::del %Batch_Path%\REST_VVV_PPP.sql
+::del %Batch_Path%\REST_TTT.sql
+::del %Batch_Path%\Telegram.Bot.dll
+::del %File_Loc%
 
 pause
