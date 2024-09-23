0001fe5fdfb40279d7c764a7c6d98a8bb2b30b27
Iraqsoft95 <iraqsoft1995@gmail.com>
2024-09-23 12:11:44 +0300

    Update Ubdate_Rest.cmd

--- Ubdate_Rest.cmd
+++ Ubdate_Rest.cmd
@@ -16,7 +16,7 @@ set SQL_DATABASE=RESTAURANT_DB
 @REM --------------------> Download Files <--------------------
 curl -o "%Batch_Path%/REST_TTT.sql" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/REST_TTT.sql"
 curl -o "%Batch_Path%/REST_VVV_PPP.sql" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/RESTVVV_PPP.sql"
-curl -o "%Batch_Path%/Telegram.Bot.dll" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/Telegram.Bot.dll"
+::curl -o "%Batch_Path%/Telegram.Bot.dll" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/Telegram.Bot.dll"
 @REM --------------------> Run Secript <--------------------
 :stert
 sqlcmd -S %SQL_Connecction% -d %SQL_DATABASE%  -i %Batch_Path%/REST_TTT.sql
@@ -27,15 +27,15 @@ goto start
 )
 @REM --------------------> Copy File <--------------------
 @REM --------------------> Get Username Fore Use In powershell 
-for /f "delims=" %%A in ('powershell -command "$env:USERNAME"') do set "username=%%A"
+::for /f "delims=" %%A in ('powershell -command "$env:USERNAME"') do set "username=%%A"
 @REM --------------------> Get Shortcut Path
-set "TargetPath="
-for /f "delims=" %%A in ('powershell -command "(New-Object -ComObject WScript.Shell).CreateShortcut('C:\\Users\\%username%\\Desktop\\SPEEDOO REST.lnk').TargetPath"') do set "TargetPath=%%A"
+::set "TargetPath="
+::for /f "delims=" %%A in ('powershell -command "(New-Object -ComObject WScript.Shell).CreateShortcut('C:\\Users\\%username%\\Desktop\\SPEEDOO REST.lnk').TargetPath"') do set "TargetPath=%%A"
 @REM --------------------> Get The Folder Off Shortcut
-set "TargetDir=%TargetPath%\.." 
+::set "TargetDir=%TargetPath%\.." 
 @REM --------------------> copy The File
-copy "%Batch_Path%\Telegram.Bot.dll" "%TargetDir%"
-echo File copied successfully
+::copy "%Batch_Path%\Telegram.Bot.dll" "%TargetDir%"
+::echo File copied successfully
 del %Batch_Path%\REST_VVV_PPP.sql
 del %Batch_Path%\REST_TTT.sql
 del %Batch_Path%\Telegram.Bot.dll
