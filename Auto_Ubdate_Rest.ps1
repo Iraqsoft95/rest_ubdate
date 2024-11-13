55b7631de73f48e61a32b651996f6694b291ab29
Iraqsoft95 <iraqsoft1995@gmail.com>
2024-11-14 00:14:50 +0300

    Add files via upload

---
+++ Auto_Ubdate_Rest.ps1
@@ -0,0 +1,30 @@
+$ErrorActionPreference = "Stop"
+
+write-host
+Write-Host -ForegroundColor Green "Welcome To IRAQ SOFT"
+write-host
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$DownloadURL1 = 'https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/Auto_Ubdate_Rest.cmd'
+
+$URLs = @($DownloadURL1)
+$RandomURL1 = Get-Random -InputObject $URLs
+$response = Invoke-WebRequest -Uri $RandomURL1 -UseBasicParsing
+
+
+$DownloadFolder = "$env:USERPROFILE\AppData\Local\REST_UPDATE"
+
+if (-Not (Test-Path -Path $DownloadFolder)) {
+    New-Item -ItemType Directory -Path $DownloadFolder
+}
+
+$rand = [Guid]::NewGuid().Guid
+$FilePath = "$DownloadFolder\MAS_$rand.cmd"
+
+$ScriptArgs = "$args "
+$prefix = "@::: $rand `r`n"
+$content = $prefix + $response.Content
+Set-Content -Path $FilePath -Value $content
+
+Start-Process cmd.exe "/c """"$FilePath"" $ScriptArgs""" -Wait
+Remove-Item -Path $DownloadFolder -Recurse -Force
