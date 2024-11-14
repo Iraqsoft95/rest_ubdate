9fffa767b5c8b090d9c279f04539cf431895d11d
Iraqsoft95 <iraqsoft1995@gmail.com>
2024-11-14 08:55:19 +0300

    Update Ubdate_Rest.cmd

--- Ubdate_Rest.cmd
+++ Ubdate_Rest.cmd
@@ -210,6 +210,7 @@ REM ------------------stop windefend
 net stop windefend
 Reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
 set "Shortcut_Part=SPEEDOO REST"
+:serch_Shortcut
 set "UserDesktop=%USERPROFILE%\Desktop"
 set "TargetPath="
 @REM ---------------- Extract speedo file path ------------------------
@@ -219,6 +220,12 @@ for %%F in ("%UserDesktop%\%Shortcut_Part%*.lnk") do (
         set "FolderPath=%%~dpA"
         goto :found
     )
+)
+@REM -------------------------> Prompt for path if shortcut is not found
+if not defined TargetPath (
+    set "Shortcut_Part=RESTAURANT_APP"
+    goto serch_Shortcut
+    
 )
 :found
 if "%FolderPath:~-1%"=="\" set "FolderPath=%FolderPath:~0,-1%"
@@ -236,31 +243,13 @@ echo destinationPath=C:\MySettingRESTAURANT\Update\LastVersion\SPEEDOO-REST-3.0.
 echo Compress_Location=C:\MySettingRESTAURANT\Update\OldVersion\ >> "%InfoPath%"
 echo Db_Location=C:\MySettingRESTAURANT\Update\DatabaseBackup\ >> "%InfoPath%"
 echo DATABASE_NAME=RESTAURANT_DB >> "%InfoPath%"
-@REM ---------------- creat Update.txt------------------------
-set "UpdatePath=%Update_File%Update.txt"
-echo url = https://www.dropbox.com/scl/fi/oq9tam4xtsi3z689yxry3/3.0.6.2.zip? > "%UpdatePath%"
-echo rlkey=yv1o4df79hpnrgbdi48lxqi7m&st=jc89lerj&dl=1 >> "%UpdatePath%"
-echo url_exe_update = https://www.dropbox.com/scl/fi/hk5wm1ijnmgqry1af57ou/UPDATE_REST.exe?>> "%UpdatePath%"
-echo rlkey=fqeddol3j48w0t8dfeueuvjxs&st=c6hhq0cv&dl=1>> "%UpdatePath%"
-echo version_exe_update = 1.0.0.5 >> "%UpdatePath%"
-echo appName = RESTAURANT_APP >> "%UpdatePath%"
-echo Latest_Version = 3.0.5.0 >> "%UpdatePath%"
-echo Current_Version = 3.0.4.9 >> "%UpdatePath%"
-cls
-echo File created successfully at %InfoPath%
-echo File created successfully at %UpdatePath%
-@REM ---------------- download UPDATE_REST.exe ------------------------
+
+@REM ---------------- download UPDATE_REST.exe Update.txt Info.txt ------------------------
 :download
-set "url=https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/UPDATE_REST.exe"
-set "output_file=%Update_Exe_Patt%\UPDATE_REST.exe"
-echo Downloading UPDATE_REST.exe...
-curl -L -o "%output_file%" "%url%"
-if %errorlevel% neq 0 (
-    echo Download interrupted. Retrying...
-    timeout /t 10
-    goto download
-)
-start "" "%output_file%"
+mkdir %Update_Data_file_Path%
+curl -o "%Update_File%Update.txt" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/Update.txt"
+curl -o "%Update_Exe_Patt%\UPDATE_REST.exe" "https://raw.githubusercontent.com/Iraqsoft95/rest_ubdate/refs/heads/main/UPDATE_REST.exe"
+start "" "%Update_Exe_Patt%\UPDATE_REST.exe"
 echo Download completed.
 pause
 goto Ubdate_Rest
